4b0081e23c25e8b597a3313b1c3b46c9feb2d13b
Dodowingster/Aqqil <aqqil.azman99@gmail.com>
2025-04-28 13:50:11 +0800

    revert to initial commit

--- js/deckManager.js
+++
@@ -1,5 +0,0 @@
-async function loadDeck(deckType) {
-    const response = await fetch(`assets/decks/${deckType}.json`);
-    const data = await response.json();
-    return data;
-}
--- js/gameLogic.js
+++
@@ -1,6 +0,0 @@
-let currentPlayer = 1;
-
-function endTurn() {
-    currentPlayer = (currentPlayer % 3) + 1; // Example for 3 players
-    document.getElementById('turn-output').innerText = "Current Player: Player " + currentPlayer;
-}
